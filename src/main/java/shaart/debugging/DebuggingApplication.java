5bda50ac60b3c2a7492013195f37038968e99670
Artur Shalaev <imshaart@gmail.com>
2019-11-06 22:50:36 +0300

    initial commit

---
+++ src/main/java/shaart/debugging/DebuggingApplication.java
@@ -0,0 +1,13 @@
+package shaart.debugging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebuggingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DebuggingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/shaart/debugging/DebuggingApplicationTests.java
@@ -0,0 +1,13 @@
+package shaart.debugging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebuggingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
